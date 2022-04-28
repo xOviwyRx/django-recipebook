92f8ccd85f10433f6e40da6e004ea94212d99934
Andrey Yurov <a.yurov6@gmail.com>
2022-04-28 20:11:41 +0400

    Changed static file's location

--- bookrecipes/bookrecipes/settings.py
+++ bookrecipes/bookrecipes/settings.py
@@ -128,9 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "bookrecipes/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "bookrecipes/static/",
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
