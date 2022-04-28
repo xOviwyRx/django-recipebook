e74cbcdd688c8844acef6d190228fb6d4373b28e
Andrey Yurov <a.yurov6@gmail.com>
2022-04-28 20:01:05 +0400

    Changed static file's location

--- bookrecipes/bookrecipes/settings.py
+++ bookrecipes/bookrecipes/settings.py
@@ -17,10 +17,9 @@ import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ROOT_DIR = environ.Path(__file__) - 4
+ROOT_DIR = environ.Path(__file__) - 3
 env = environ.Env()
 env.read_env(ROOT_DIR(".env"))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -130,6 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "bookrecipes/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- bookrecipes/main/views.py
+++ bookrecipes/main/views.py
@@ -14,22 +14,29 @@ def index(request):
 def recipes(request):
     """Recipe list with search by name and ingredient"""
     recipes_objects = Recipe.objects.all()
-    form_search = NameFilterForm(request.GET)
-    if form_search.is_valid():
-        if form_search.cleaned_data["recipe_name"]:
-            recipes_objects = recipes_objects. \
-                filter(name__icontains=form_search.cleaned_data["recipe_name"])
-        if form_search.cleaned_data["ingredient_name"]:
-            recipes_objects = recipes_objects. \
-                filter(ingredients__name__icontains=form_search.cleaned_data["ingredient_name"])
-    return render(
-        request,
-        "main/index.html",
-        {
-            "recipes": recipes_objects,
-            "form": {
-                "description": "Here you can see recipe list",
+    if len(recipes_objects):
+        form_search = NameFilterForm(request.GET)
+        if form_search.is_valid():
+            if form_search.cleaned_data["recipe_name"]:
+                recipes_objects = recipes_objects. \
+                    filter(name__icontains=form_search.cleaned_data["recipe_name"])
+            if form_search.cleaned_data["ingredient_name"]:
+                recipes_objects = recipes_objects. \
+                    filter(ingredients__name__icontains=form_search.cleaned_data["ingredient_name"])
+        return render(
+            request,
+            "main/index.html",
+            {
+                "recipes": recipes_objects,
+                "form": {
+                    "description": "Here you can see recipe list",
+                },
+                "form_search": form_search
             },
-            "form_search": form_search,
-        },
-    )
+        )
+    else:
+        "No any recipe - show base.html template"
+        return render(
+            request, "base.html"
+        )
+
